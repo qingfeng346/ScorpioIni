560844803aad69c371b641545ec9f2dce6144944
while <while@while.net.cn>
2017-10-13 17:04:02 +0800

    修改注释方式 改成 ; # // 三种

--- ScorpioIni/ScorpioIni.cs
+++ ScorpioIni/ScorpioIni.cs
@@ -4,8 +4,12 @@ using System.Text;
 using System.IO;
 /// <summary> 单个模块数据 </summary>
 public class ScorpioIniSection {
+    public string section = "";
     /// <summary> 所有数据 </summary>
     public Dictionary<string, ScorpioIniValue> datas = new Dictionary<string, ScorpioIniValue>();
+    public ScorpioIniSection(string section) {
+        this.section = section;
+    }
     /// <summary> 设置值 </summary>
     public void Set(string key, string value, string comment) {
         if (!datas.ContainsKey(key))
@@ -41,6 +45,7 @@ public class ScorpioIniValue {
 }
 /// <summary> 读取ini文件 </summary>
 public class ScorpioIni {
+    public const string HintString = "注释使用 # ; //  数据格式为 key=value, 不支持回车";
     /// <summary> 所有数据 </summary>
     public Dictionary<string, ScorpioIniSection> m_ConfigData = new Dictionary<string, ScorpioIniSection>();
     /// <summary> 构造函数 </summary>
@@ -72,53 +77,28 @@ public class ScorpioIni {
     public void InitFormString(string buffer) {
         try {
             m_ConfigData.Clear();
-            string[] datas = buffer.Split('\n');
-            string section = "";
-            bool startComment = false;
-            StringBuilder comment = new StringBuilder();
+            string[] datas = buffer.Split('\n');    //所有行数据
+            string section = "";                    //区间
+            string comment = "";                    //注释
             int count = datas.Length;
             for (int i = 0; i < count; ++i) {
                 string data = datas[i].Trim();
                 if (!string.IsNullOrEmpty(data)) {
-                    // /* */为区域注释
-                    if (data.StartsWith("/*")) {
-                        comment.Append(data.Replace(@"/*", "").Replace(@"*/", ""));
-                        if (data.EndsWith("*/")) {
-                            startComment = false;
-                            continue;
-                        }
-                        startComment = true;
+                    // [#] [;] [//] 开头都可以注释单行注释
+                    if (data.StartsWith("//") || data.StartsWith("#") || data.StartsWith(";")) {
+                        comment = data;
                         continue;
                     }
-                    // */ 为区域注释结尾
-                    else if (data.EndsWith("*/")) {
-                        comment.Append(data.Replace(@"/*", "").Replace(@"*/", ""));
-                        startComment = false;
-                        continue;
-                    }
-                    // //为行注释
-                    else if (data.StartsWith("//")) {
-                        comment.Append(data.Replace(@"//", ""));
-                        continue;
-                    }
-                    if (startComment == true) {
-                        continue;
-                    }
-                    if (data.StartsWith("[")) {
-                        int indexLeft = data.IndexOf("[");
-                        int indexRight = data.IndexOf("]");
-                        if (indexLeft >= 0 && indexRight >= 0) {
-                            section = data.Substring(indexLeft + 1, indexRight - indexLeft - 1);
-                        }
+                    if (data.StartsWith("[") && data.EndsWith("]")) {
+                        section = data.Substring(1, data.Length - 2);
                     } else {
                         int index = data.IndexOf("=");
                         if (index >= 0) {
                             string key = data.Substring(0, index).Trim();
                             string value = data.Substring(index + 1).Trim();
                             Set(section, key, value, comment.ToString());
-                            comment = new StringBuilder();
                         } else {
-                            throw new Exception((i + 1) + " 行填写错误, 正确格式为 key=value 不支持回车");
+                            throw new Exception((i + 1) + " 行填写错误, " + HintString);
                         }
                     }
                 }
@@ -178,7 +158,7 @@ public class ScorpioIni {
     /// <summary> 设置Value </summary>
     public void Set(string section, string key, string value, string comment) {
         if (!m_ConfigData.ContainsKey(section))
-            m_ConfigData.Add(section, new ScorpioIniSection());
+            m_ConfigData.Add(section, new ScorpioIniSection(section));
         m_ConfigData[section].Set(key, value, comment);
     }
     /// <summary> 删除Key </summary>
@@ -208,24 +188,23 @@ public class ScorpioIni {
         SortedDictionary<string, ScorpioIniSection> data = new SortedDictionary<string, ScorpioIniSection>(m_ConfigData);
         StringBuilder builder = new StringBuilder();
         if (data.ContainsKey("")) {
-            foreach (var val in data[""].datas) {
-                var configValue = val.Value;
-                if (!string.IsNullOrEmpty(configValue.comment))
-                    builder.AppendLine(string.Format("/*{0}*/", configValue.comment));
-                builder.AppendLine(string.Format("{0}={1}", val.Key, configValue.value));
-            }
+            Content(builder, data[""]);
         }
         foreach (var pair in data) {
             if (string.IsNullOrEmpty(pair.Key))
                 continue;
-            builder.AppendLine(string.Format("[{0}]", pair.Key));
-            foreach (var val in pair.Value.datas) {
-                var configValue = val.Value;
-                if (!string.IsNullOrEmpty(configValue.comment))
-                    builder.AppendLine(string.Format("/*{0}*/", configValue.comment));
-                builder.AppendLine(string.Format("{0}={1}", val.Key, configValue.value));
-            }
+            Content(builder, pair.Value);
         }
         return builder.ToString();
+    }
+    public void Content(StringBuilder builder, ScorpioIniSection section) {
+        if (!string.IsNullOrEmpty(section.section))
+            builder.AppendLine(string.Format("[{0}]", section.section));
+        foreach (var data in section.datas) {
+            var value = data.Value;
+            if (!string.IsNullOrEmpty(value.comment))
+                builder.AppendLine(value.comment);
+            builder.AppendLine(string.Format("{0}={1}", data.Key, value.value));
+        }
     }
 }
